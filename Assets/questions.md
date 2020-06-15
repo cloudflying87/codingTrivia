bd9bd80a407159291c7a1bf3c0d8474dbab9b3e9
David Hale <davidhale87@gmail.com>
2020-06-15 16:24:03 -0500

    Big commit. I have gotten the questions working in the correct place. Added three questions to the word bank. Added all the event listeners for the button clicks. Have the check answer working and random questions being chosen. Currently working on the end game function.

--- Assets/questions.md
+++ Assets/questions.md
@@ -1,10 +1,14 @@
-What are the CSS properties that are used to add space around sections of content?
-
-1.  <Padding>
-2.  Cleaner
-3.  Break
-4.  Spacing
-
+QuestionBank = [
+    { Question: What are the CSS properties that are used to add space around sections of content?,
+    Answers:{
+        a: <Padding>
+        b: Cleaner
+        c:  Break
+        d:  'Spacing'
+    }
+    correctAnswer: 'a'
+    }
+]
 What is the name given to the CSS element that always starts on a new-line and takes up the entire width available to it?
 
 1.  Spacer
@@ -12,33 +16,12 @@ What is the name given to the CSS element that always starts on a new-line and t
 3.  Margin
 4.  <Block-level>
 
-What is the most important CSS property, used for controlling the layout?
-
-1.  <Display>
-2.  Table
-3.  Margin
-4.  div
-
 CSS stands for ____ Style Sheets.
 
 1.  Concept
 2.  Curious
 3.  <Cascading>
 4.  Concave
-
-All HTML elements are considered what?
-
-1.  Objects
-2.  Code
-3.  Tables
-4.  <Boxes>
-
-What is the name of the group of properties that allows you to control the height and width of elements?
-
-1.  Box
-2.  Block
-3.  Size
-4.  <Dimension>
 
 What is the value called that defines colors such as the following: #FFFF00?
 
@@ -67,3 +50,101 @@ What property is used to specify whether or not an element should appear as if i
 2.  Jumper
 3.  Top Level
 4.  Foreground
+
+What tag is used to define a container for an external app or plug-in?
+
+1.  <embed>
+2.  <code>
+3.  <!DOCTYPE>
+4.  <caption>
+
+What tag is used to define an unordered list that is bulleted?
+
+1.  <ul>
+2.  <s>
+3.  <li>
+4.  <u>
+
+What tag is used to define a list item (in a bulleted list)?
+
+1.  <li>
+2.  <ul>
+3.  <s>
+4.  <u>
+
+What tag is used to define an interactive field where users can enter data?
+
+1.  <input>
+2.  <datalist>
+3.  <enterpoint>
+4.  <dialog>
+
+What group of tags are used to define the text headers in the body of the HTML document?
+
+1.  <h1> to <h6>
+2.  <td>
+3.  <footer>
+4.  <button>
+
+What tag can be used to insert a line break or blank line in an HTML document?
+
+1.  <br></br>
+2.  <body></body>
+3.  <head></head>
+4.  <title></title>
+
+What tag is used to render or transform text into an emphasized (italics) version?
+
+1.  <em>
+2.  <strong>
+3.  <a>
+4.  <blockquote>
+
+What is the name of the object that allows you to perform mathematical tasks with the interpreter?
+
+1.  Solve
+2.  Number
+3.  Count
+4.  Math
+
+What is the element used – and hidden – in code that explains things and makes the content more readable?
+
+1.  Comparisons
+2.  Quotations
+3.  Comments
+4.  Notes
+
+What are the identifiers called that cannot be used as variables or function names?
+
+1.  Constants
+2.  Concrete Terms
+3.  Favorites
+4.  Reserved Words
+
+In JavaScript, what element is used to store multiple values in a single variable?
+
+1.  Variables
+2.  Arrays
+3.  Strings
+4.  Functions
+
+In JavaScript, what is used in conjunction with HTML to “react” to certain elements?
+
+1.  Events
+2.  RegExp
+3.  Boolean
+4.  Condition
+
+What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?
+
+1.  Debugger
+2.  Clone
+3.  Repeater
+4.  Loop
+
+What is the name of the statement that is used to exit or end a loop?
+
+1.  Falter statement
+2.  Break statement
+3.  Conditional statement
+4.  Close statement
--- script.js
+++ script.js
@@ -12,31 +12,172 @@
             // Incorrect answers
             // score
             // Box to enter high score
+
 // Variables
 // Objects for the quiz questions
+    var questionBank
+    var questionShuffle, currentQuestionNum
+    var answerCorrect
+    
+    questionBank = [
+        {question:'What are the CSS properties that are used to add space around sections of content?',
+            answers:[
+                {text:'Padding', correct:true},
+                {text:'Cleaner', correct:false},
+                {text:'Break',correct:false},
+                {text:'Spacing',correct:false},
+            ]
+        },
+        {question:'What is the name given to the CSS element that always starts on a new-line and takes up the entire width available to it?',
+            answers:[
+                {text:'Spacer',correct:false},
+                {text:'Line',correct:false},
+                {text:'Margin' , correct:false},
+                {text:'Block-level',correct:true},
+            ]
+        },
+        {question: 'CSS stands for ____ Style Sheets.',
+            answers:[
+                {text:'Concept',correct:false},
+                {text:'Curious',correct:false},
+                {text:'Cascading',correct:true},
+                {text:'Concave',correct:false},
+            ]
+        },
+        {question:'What tag is used to define a container for an external app or plug-in?',
+            answers:[
+                {text:'<embed>',correct:true},
+                {text:'<code>',correct:false},
+                {text:'<!DOCTYPE>',correct:false},
+                {text:'<caption>',correct:false},
+            ]
+        }
+    ]
 // Correct answers
-    var correctans
+    var correctAns
+    var correctAnswer
+    var countQuestion
 // Incorrect answers'
     var incorrectans
 // Timer
     var timer
+    var timeLeft = 60
+    var timeInterval
 // each part of the page
-    var timerdisplay = document.getElementById("timer")
-    var highscores = document.querySelector(".highscores")
-    var title = document.querySelector("#title")
-    var question =document.querySelector(".question")
-    var button = document.querySelector("#startBtn")
-    var answer = document.querySelector(".answer")
+    var timerDisplay = document.getElementById('timer');
+    var highscores = document.getElementById('highScores');
+    var title = document.getElementById('title');
+    var question =document.getElementById('question');
+    var startBtn = document.getElementById('startBtn');
+    var correctButton
+    var ans1 = document.getElementById('ans1');
+    var ans2 = document.getElementById('ans2');
+    var ans3 = document.getElementById('ans3');
+    var ans4 = document.getElementById('ans4');
 
 // Main game
-startBtn.addEventListener("click", startgame)
+startBtn.addEventListener('click', startgame);
 
 function startgame(){
-    console.log("Working")
+    startBtn.classList.add('hide')
+    timer();
+    questionShuffle = questionBank.sort(() => Math.random() - .5);
+    currentQuestionNum = 0
+    correctAns = 0
+    incorrectans = 0
+    countQuestion = 1
+    ans1.classList.remove('hide')
+    ans2.classList.remove('hide')
+    ans3.classList.remove('hide')
+    ans4.classList.remove('hide')
     
-    timerdisplay.innerText = ""
-    title.innerText =""
-    highscores.innerText = ""
-    question.innerText = ""
-    startBtn.style.display = "none"
+    setNextQuestion()
+    
 }
+
+function timer(){
+    // 60 Second timer
+    
+    var timeInterval = setInterval(function() {
+        timerDisplay.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+      if (timeLeft === 0) {
+        timerDisplay.textContent = '';
+        clearInterval(timeInterval);
+        endgame();
+      }
+    }, 1000);
+}
+
+function setNextQuestion(){
+    showQuestion(questionShuffle[currentQuestionNum])
+}
+
+var i = 0;
+
+function showQuestion (question){
+    var i = 0;
+    questionElement.textContent = question.question
+    ans1.textContent = question.answers[0].text;
+    ans2.textContent = question.answers[1].text;
+    ans3.textContent = question.answers[2].text;
+    ans4.textContent = question.answers[3].text;
+    
+    question.answers.forEach(writeAnswers);
+    
+    function writeAnswers(answers) {
+        i ++;
+        if (answers.correct === true){
+            correctButton = i;
+        } 
+    }
+}
+
+function checkanswer(number){
+    
+    if (correctButton === number){
+        answerCorrect === true
+        correctAns ++ 
+        currentQuestionNum ++
+    }
+    else {
+        answerCorrect === false
+        incorrectans ++
+        timeLeft -= 10
+        currentQuestionNum ++
+    }
+    
+    console.log(currentQuestionNum)
+    if (currentQuestionNum == questionBank.length) {
+        endgame()
+    }
+    setNextQuestion()
+}
+
+function endgame(){
+    console.log("Correct " + correctAns)
+    console.log("Wrong " + incorrectans)
+    console.log(timeLeft)
+    question.classList.add('hide')
+    ans1.classList.add('hide')
+    ans2.classList.add('hide')
+    ans3.classList.add('hide')
+    ans4.classList.add('hide')
+
+
+}
+ans1.addEventListener('click',function(){
+    checkanswer(1);
+},false);
+
+ans2.addEventListener('click',function(){
+    checkanswer(2);
+},false);
+
+ans3.addEventListener('click',function(){
+    checkanswer(3);
+},false);
+
+ans4.addEventListener('click',function(){
+    checkanswer(4);
+},false);
